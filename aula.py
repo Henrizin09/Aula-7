6177807117beb475ff69d83a423fd3d25877e778
Henrizin09 <josehenriquemota07@gmail.com>
2024-08-14 17:35:05 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,46 @@
+# import random
+# numero_aleatorio = random.randint(0,5)
+# meu_chute = int(input("Digite seu chute"))
+
+# if numero_aleatorio == meu_chute:
+#     numero_sorteado = numero_aleatorio
+#     print(numero_sorteado)
+#     print("Acertou")
+
+# elif numero_aleatorio>1:
+#     print("é um numero maior que 1...", numero_aleatorio)
+
+# else:
+#         print("errou!")
+
+
+# meu_carrinho = []
+# total_valores = []
+# produtos = ["arroz" , "feijão" , "ervilha"]
+# valores = [20.0,18.0,5.]
+
+# produto1 = input("Digite o produto 1")
+# produto2 = input("Digite o produto 2")
+# produto3 = input("Digite o produto 3")
+
+# if (poduto1, produto2, produto3) == (produto[0], produtos[1], produtos[2]):
+#     meu_carrinho += (produtos[0], produtos[1], produtos[2])
+#     print(meu_carrinho)
+# if (produto1, produto2, produto3) != produtos:
+#     print("Não existe o produto")
+
+#     if produto1 in produtos or produto2 in produtos or produto3 in produtos:
+#        meu_carrinho(produto1)
+#        meu_carrinho(produto2)
+#        meu_carrinho(produto3)
+
+# if produto1 in produtos:
+#     meu_carrinho.append(produto1)
+#     print(meu_carrinho)
+#     meu_carrinho2 = meu_carrinho
+# if produto2 in produtos:
+#     meu_carrinho,append(produto2)
+#     print(meu_carrinho2)
+#     meu_carrinho3 = meu_carrinho2
+
+
